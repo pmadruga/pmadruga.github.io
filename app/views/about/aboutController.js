17633af0614a2423a02c2388552eba4ce3f008c2
Pedro Madruga <pmadruga@gmail.com>
2015-02-24 19:24:42 +0100

    removed default test

--- app/views/about/aboutController.js
+++ app/views/about/aboutController.js
@@ -2,9 +2,5 @@
 
 angular.module('pedromadrugacom')
   .controller('AboutController', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
   });
