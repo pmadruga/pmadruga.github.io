76a43c1041d9ee185f572f43d8e3d4cb1db755f8
Pedro Madruga <pmadruga@gmail.com>
2016-03-15 23:05:14 +0100

    starting from scratch by removing redundant stuff

--- src/common/services.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/* Handles all the firebase connections */
-
-export default class firebaseConnection {
-    constructor () {
-        return new Firebase("https://radiant-fire-4389.firebaseio.com");
-    }
-}
